22d0859030f343c8c234bc9368947ac5291f5fb3
atsuki89 <atsuki.tomotsune@gmail.com>
2021-02-21 10:37:42 +0900

    バリデーションの更新とテストコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+
+  validates :password, format: { with: /\A[a-zA-Z]+\z/ }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,102 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "test3@test.com", password: "abc000000", password_confirmation: "abc000000", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
-    end
-    it "メールアドレスが必須であること" do
-      user = User.new(nickname: "furima三郎", email: "", password: "abc000000", password_confirmation: "abc000000", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
-    end
-    it "メールアドレスは、@を含む必要があること" do
-      user = User.new(nickname: "furima三郎", email: "test3test.com", password: "abc000000", password_confirmation: "abc000000", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
-    end
-    it "パスワードが必須であること" do
-      user = User.new(nickname: "furima三郎", email: "test3@test.com", password: "", password_confirmation: "", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
-    end
-    it "パスワードは、6文字以上での入力が必須であること" do
-      user = User.new(nickname: "furima三郎", email: "test3@test.com", password: "abc", password_confirmation: "abc", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
-    end
-    it "パスワードとパスワード（確認用）、値の一致が必須であること" do
-      user = User.new(nickname: "furima三郎", email: "test3@test.com", password: "abc000000", password_confirmation: "abc111111", last_name:"山田", first_name: "山太郎", last_name_kana: "ヤマダ", first_name_kana: "ヤマタロウ", birthday: "")
-      user.valid?
+
+    context '新規登録がうまくいくとき' do
+      it "情報が正しく入力されていれば登録できる" do
+        user = FactoryBot.build(:user)
+        user.valid?
+      end
     end
 
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.nickname = ''
+        user.valid?
+      end
+      it "メールアドレスが必須であること" do
+        user = FactoryBot.build(:user)
+        user.email = ''
+        user.valid?
+      end
+      it "メールアドレスは、@を含む必要があること" do
+        user = FactoryBot.build(:user)
+        user.email = 'test3test.com'
+        user.valid?
+      end
+      it "パスワードが必須であること" do
+        user = FactoryBot.build(:user)
+        user.password = ''
+        user.valid?
+      end
+      it "パスワードは、6文字以上での入力が必須であること" do
+        user = FactoryBot.build(:user)
+        user.password = 'abc'
+        user.password_confirmation = 'abc'
+        user.valid?
+      end
+      it "パスワードとパスワード（確認用）、値の一致が必須であること" do
+        user = FactoryBot.build(:user)
+        user.password_confirmation = 'abc111111'
+        user.valid?
+      end
+      it "生年月日が空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.birthday = ''
+        user.valid?
+      end
+      it "お名前(全角)lastnameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.last_name = ''
+        user.valid?
+      end
+      it "お名前(全角)firstnameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name = ''
+        user.valid?
+      end
+      it "お名前カナ(全角)lastnameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.last_name_kana = ''
+        user.valid?
+      end
+      it "お名前カナ(全角)firstnameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name_kana = ''
+        user.valid?
+      end
+      it "お名前(全角)lastnameが全角（漢字・ひらがな・カタカナ）以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.last_name = 'yamada'
+        user.valid?
+      end
+      it "お名前(全角)firstnameが全角（漢字・ひらがな・カタカナ）以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name = 'yamada'
+        user.valid?
+      end
+      it "お名前(全角)lastnameが全角（カタカナ）以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.last_name_kana = 'yamada'
+        user.valid?
+      end
+      it "お名前(全角)firstnameが全角（カタカナ）以外だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name_kana = 'yamada'
+        user.valid?
+      end
+      it "メールアドレスがすでに使用されているものだと登録できない" do
+        user = FactoryBot.build(:user)
+        user.email = 'test@test.com'
+        user.valid?
+      end
+      it "パスワードは半角英数字混合以外では登録できない" do
+        user = FactoryBot.build(:user)
+        user.password = 'ああああああああ'
+        user.password_confirmation = 'ああああああああ'
+        user.valid?
+      end
+    end
   end
 end
