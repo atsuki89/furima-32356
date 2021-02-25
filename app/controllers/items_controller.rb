33682e9781a819853c99b9f3b37a41800313e439
atsuki89 <atsuki.tomotsune@gmail.com>
2021-02-25 17:53:29 +0900

    ログイン状態別、商品出品ページへの遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sessions, only: [:new]
 
   def index
   end
   
+  def new
+  end
+
+  private
+
+  def move_to_sessions
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
