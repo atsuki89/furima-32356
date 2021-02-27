d1ad2ff466b63a01631f380d259d0a455dccb02e
atsuki89 <atsuki.tomotsune@gmail.com>
2021-02-27 12:10:24 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,16 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item =Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
   end
 
   private
@@ -13,6 +23,10 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :sipping_charges_id, :sipping_area_id, :estimated_shipping_date_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :sipping_charges
+  belongs_to :sipping_area
+  belongs_to :estimated_shipping_date
+
+  validates :name, :description, :price, :category_id, :condition_id, :sipping_charges_id, :sipping_area_id, :estimated_shipping_date_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :sipping_charges_id, numericality: { other_than: 1 }
+  validates :sipping_area_id, numericality: { other_than: 1 }
+  validates :estimated_shipping_date_id, numericality: { other_than: 1 }
+
   private
 
   def item_params
